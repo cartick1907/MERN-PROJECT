8bac3c3c984dc70ee17f68880cfe13d322c47e7c
cartick1907 <kartikbhatt1299@gmail.com>
2023-10-12 11:47:22 +0530

    added frontend authentication

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,59 +1,99 @@
-import {useState, useEffect} from 'react'
-import {FaSignInAlt, FaUser} from 'react-icons/fa'
+import { useState, useEffect } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { login, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
-  const [formData, setFormData ] = useState({
-    email :'',
-    password :'',
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
   })
-  const {name, email, password, password2} = formData
-  const onChange =(e)=>{
 
-    setFormData((prevstate)=>({
+  const { email, password } = formData
 
-      ...prevstate,
-      [e.target.name]:e.target.value
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
     }))
-
   }
 
-  const onSubmit =(e)=>{
+  const onSubmit = (e) => {
     e.preventDefault()
+
+    const userData = {
+      email,
+      password,
+    }
+
+    dispatch(login(userData))
   }
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
   return (
-    
     <>
       <section className='heading'>
         <h1>
-          <FaSignInAlt/> Login
+          <FaSignInAlt /> Login
         </h1>
-        <p>Please enter Credentials</p>
+        <p>Login and start setting goals</p>
       </section>
+
       <section className='form'>
         <form onSubmit={onSubmit}>
           <div className='form-group'>
-          <input type="email"
-          className='form-control'
-          id='email'
-          name='email'
-          value={email}
-          placeholder='enter your email'
-          onChange={onChange}
-           />
-          <input type="password"
-          className='form-control'
-          id='password'
-          name='password'
-          value={password}
-          placeholder='enter your password'
-          onChange={onChange}
-           />
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              placeholder='Enter your email'
+              onChange={onChange}
+            />
           </div>
           <div className='form-group'>
-            <button type='submit' className='btn btn-block'>Submit</button>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              placeholder='Enter password'
+              onChange={onChange}
+            />
           </div>
-          
+
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>
+              Submit
+            </button>
+          </div>
         </form>
       </section>
     </>
